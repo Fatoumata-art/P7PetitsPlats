89fa885195179a012e9a67c77f6518a4ceb5c350
fatoumata-art <lybatoyly@live.fr>
2023-11-02 14:18:56 +0100

    improve code

--- scripts/index.js
+++ scripts/index.js
@@ -15,26 +15,6 @@ function init(){
   const ustensil = filterUstensils();
   listUstensil(recipes);
  
-  //search input select option
-  const optionList = document.querySelector('list_option li');
-  const optiontSearches = document.querySelectorAll('#optionSearch');
-  optiontSearches.forEach(optiontSearch =>
-    
-    optiontSearch.addEventListener('keyup', function(){
-      let filter, li, i, textValue;
-      filter = optiontSearch.value.toUpperCase();
-      li = optionList.getElementsByTagName(li);
-      for( i = 0; i < li.length; i++){
-        liCount = li[i];
-        textValue = liCount.textContent;
-        if(textValue.toUpperCase().indexOf(filter) > -1){
-          li[i].style.display = '';
-        }else{
-          li[i].style.display = 'none';
-        }
-      }
-    })
-    )
   
 
   //Total recettes
